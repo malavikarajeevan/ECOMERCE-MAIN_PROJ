9c232066f5349119db75f4b01c8c344196c23526
malavikarajeevan <malavikarajeevan85@gmail.com>
2024-02-08 10:50:16 +0530

    new file added

---
+++ Todoproject/cartapp/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render,redirect
+from TodoApp.models import Product
+from .models import Cart
+from django.core.exceptions import ObjectDoesNotExist
+
+# Create your views here.
+def addcart(req,id):
+    user=req.session['user']
+    product=Product.objects.get(id=id)
+    print(product)
+    try:
+        cart=Cart.objects.get(product=product)
+        if cart.quantity<cart.product.stock:
+            cart.quantity+=1
+            cart.save()
+    except Cart.DoesNotExist:
+        cart=Cart.objects.create(user=user,product=product,quantity=1)
+    return redirect('cartapp:displaycart')
+    
+def displaycart(req):
+    user=req.session['user']
+    cart=Cart.objects.all().filter(user=user)
+    return render(req,'cart.html',{'cart':cart})
+def removecart(req,id):
+    user=req.session['user']
+    product=Product.objects.get(id=id)
+    cart=Cart.objects.get(product=product,user=user)
+    if cart.quantity>1:
+        cart.quantity-=1
+        cart.save()
+    return redirect('cartapp:displaycart')
+
+def fullremove(req,id):
+    user=req.session['user']
+    product=Product.objects.get(id=id)
+    cart=Cart.objects.get(product=product,user=user)
+    cart.delete()
+    return redirect('cartapp:displaycart')     
